ddeb84e8b5ae1d8e63dec7d37b1638fe2f7ae951
bigzhu <vermiliondun@gmail.com>
2025-01-16 13:05:00 +0800

    改用 title 作为文件名

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -20,5 +20,34 @@ return {
         path = "~/Sync/home/cheese",
       },
     },
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    ---使用 title 作为 node 的文件名
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
+    mappings = {
+      -- 在visual模式下按回车键时创建链接
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
   },
 }
