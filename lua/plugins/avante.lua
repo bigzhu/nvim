0a01a52dc031341632cdac07b356c1aec1c5125d
bigzhu <vermiliondun@gmail.com>
2025-06-01 14:28:32 +0800

    hide file

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -6,48 +6,49 @@ return {
   opts = {
     -- add any opts here
     provider = "gemini",
-    vendors = {
-      deepseek = {
-        __inherited_from = "openai",
-        api_key_name = "DEEPSEEK_API_KEY",
-        endpoint = "https://api.deepseek.com",
-        model = "deepseek-coder",
-      },
-    },
+    -- vendors = {
+    --   deepseek = {
+    --     __inherited_from = "openai",
+    --     api_key_name = "DEEPSEEK_API_KEY",
+    --     endpoint = "https://api.deepseek.com",
+    --     model = "deepseek-coder",
+    --   },
+    -- },
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  -- 如果您想从源代码构建，请执行 `make BUILD_FROM_SOURCE=true`
   build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- 对于 Windows
   dependencies = {
+    "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    --- 以下依赖项是可选的，
+    "echasnovski/mini.pick", -- 用于文件选择器提供者 mini.pick
+    "nvim-telescope/telescope.nvim", -- 用于文件选择器提供者 telescope
+    "hrsh7th/nvim-cmp", -- avante 命令和提及的自动完成
+    "ibhagwan/fzf-lua", -- 用于文件选择器提供者 fzf
+    "nvim-tree/nvim-web-devicons", -- 或 echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- 用于 providers='copilot'
     {
-      -- support for image pasting
+      -- 支持图像粘贴
       "HakonHarnes/img-clip.nvim",
       event = "VeryLazy",
       opts = {
-        -- recommended settings
+        -- 推荐设置
         default = {
           embed_image_as_base64 = false,
           prompt_for_file_name = false,
           drag_and_drop = {
             insert_mode = true,
           },
-          -- required for Windows users
+          -- Windows 用户必需
           use_absolute_path = true,
         },
       },
     },
     {
-      -- Make sure to set this up properly if you have lazy=true
+      -- 如果您有 lazy=true，请确保正确设置
       "MeanderingProgrammer/render-markdown.nvim",
       opts = {
         file_types = { "markdown", "Avante" },
