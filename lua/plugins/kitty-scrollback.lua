259869313c61eb471dc2f9871d20ce1f40947c9b
bigzhu <vermiliondun@gmail.com>
2025-07-26 12:22:14 +0800

    避免弹出 past window

--- lua/plugins/kitty-scrollback.lua
+++ lua/plugins/kitty-scrollback.lua
@@ -8,16 +8,20 @@ return {
   -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
   config = function()
     require("kitty-scrollback").setup({
-      -- 设置解决退出时保存提示的问题
-      callbacks = {
-        after_ready = function()
-          -- 设置缓冲区为只读和未修改状态，避免保存提示
-          vim.bo.readonly = true
-          vim.bo.modified = false
-          vim.bo.modifiable = false
-        end,
+      {
+        paste_window = {
+          yank_register_enabled = false,
+        },
+        -- 设置解决退出时保存提示的问题
+        callbacks = {
+          after_ready = function()
+            -- 设置缓冲区为只读和未修改状态，避免保存提示
+            vim.bo.readonly = true
+            vim.bo.modified = false
+            vim.bo.modifiable = false
+          end,
+        },
       },
     })
   end,
 }
-
