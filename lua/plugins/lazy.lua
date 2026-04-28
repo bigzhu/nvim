a7eae77dfb5b8de62cd9259ea3c6aeaeb25d9df1
bigzhu <vermiliondun@gmail.com>
2026-04-28 09:31:50 +0800

    chore(config): update theme and spell list

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -1,10 +1,18 @@
 return {
-  { "rebelot/kanagawa.nvim" },
-  -- Configure LazyVim to load kanagawa
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+  -- Configure LazyVim to load gruvbox-material
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "gruvbox-material",
     },
   },
   -- add more treesitter parsers
